88d65480db20fe1720f7484ec929ec8aff9428cd
Kartikaya Gupta <kats@trevize.staktrace.com>
2021-04-12 06:42:34 -0400

    Update example for less verbose output when bulk-scanning

--- examples/classfile.rs
+++ examples/classfile.rs
@@ -7,9 +7,10 @@ fn main() {
         let mut file = File::open(&arg).unwrap();
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).unwrap();
-        println!("Dumping {:?}", arg);
-        let class = cafebabe::parse_class(&bytes).unwrap();
-        println!("{:?}", class.this_class);
-        println!("{:?}", class);
+        println!("Parsing {:?}...", arg);
+        match cafebabe::parse_class(&bytes) {
+            Ok(class) => println!("Successfully parsed {:?}", class.this_class),
+            Err(e) => println!("Error: {:?}", e),
+        };
     }
 }
